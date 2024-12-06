32e3a639ff2369a8bd83e2c24d23fffbd4816856
jonsnow32 <jonsnow.cplus@gmail.com>
2024-12-06 10:13:21 +0700

    Apply the VVF Gradle plugin to the build file extension.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 pluginManagement {
   repositories {
+    mavenLocal()
     google {
       content {
         includeGroupByRegex("com\\.android.*")
@@ -8,6 +9,7 @@ pluginManagement {
       }
     }
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
     gradlePluginPortal()
   }
 }
@@ -15,6 +17,7 @@ pluginManagement {
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
+    mavenLocal()
     google()
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
@@ -22,5 +25,15 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "sampleExt"
-include(":app")
-include(":jar")
+
+val disabled = listOf<String>()
+
+File(rootDir, ".").eachDir { dir ->
+  if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+    include(dir.name)
+  }
+}
+
+fun File.eachDir(block: (File) -> Unit) {
+  listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
